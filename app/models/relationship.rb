7e6e7dd3adf9a50f0d78fcc6653d4688931f6c88
bjkillen <blaine.j.killen@gmail.com>
2012-10-13 12:59:43 -0400

    Kinda Working

---
+++ app/models/relationship.rb
@@ -0,0 +1,5 @@
+class Relationship < ActiveRecord::Base
+attr_accessible :follower_id, :followed_id
+
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  attr_accessible :name, :password, :role
+
+  has_many :events
+  has_many :follows, :foreign_key => :follower_id, :dependent => :destroy
+
+  validates :name, :presence => true, :uniqueness => true
+  validates :password, :presence => true, :confirmation => true
+  validates :role, :presence => true
+
+end
