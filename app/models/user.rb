ce218081f6413e5e795f10c6fb4a5b2302f5addf
bjkillen <blaine.j.killen@gmail.com>
2012-10-13 15:42:07 -0400

    DB migrate working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :name, :password, :role
 
   has_many :events
-  has_many :follows, :foreign_key => :follower_id, :dependent => :destroy
+  has_many :relationships, :foreign_key => 'follower_id', :dependent => :destroy
 
   validates :name, :presence => true, :uniqueness => true
   validates :password, :presence => true, :confirmation => true
--- db/migrate/20121013162859_create_relationships.rb
+++ db/migrate/20121013162859_create_relationships.rb
@@ -6,11 +6,12 @@ class CreateRelationships < ActiveRecord::Migration
 
       t.timestamps
     end
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], unique: true
   end
 
-  add_index :relationships, :follower_id
-  add_index :relationships, :followed_id
-  add_index :relationships, [:follower_id, :followed_id], unique: true
+
 
   def self.down
     drop_table :relationships
