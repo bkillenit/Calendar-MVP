1b3d8d1870e4bbcf922ca6d0cb00d38bc5a4f3af
bjkillen <Rosco001>
2012-11-08 17:11:04 -0500

    Event loading from the correct source, events controller corrected

--- public/javascripts/jquery-extras.js
+++ public/javascripts/jquery-extras.js
@@ -11,22 +11,22 @@ function merge_user(user_id)
 
         //failsafe to check if the box is checked when the function is being called
         if ($(mergebox).is(":checked")) {
-        //changes calendars source
-        var source = "/users/" + user_id.toString() + "/events";
+        //changes calendars source and adjust the events loaded with options
+        var source = "/users/" + user_id + "/events";
         $('#calendar').fullCalendar('addEventSource', {
             url: source,
-            className: 'shadows',
+            className: 'merged-event',
             editable: false
         });
 
         //changes the toggle boxes color to gray and adjust text to Unmerge
         var divbox = "#" + user_id;
-        $(divbox.toString()).css("backgroundColor", "#f2f2f2");
-        $(divbox.toString()).find("p").text('Unmerge');
+        $(divbox).css("backgroundColor", "#f2f2f2");
+        $(divbox).find("p").text('Unmerge');
 
         //changes function of checkbox to unmerge
         var checkbox = "#mergebox" + user_id;
-        var function_id = "unmerge_user(" + user_id.toString() + ")";
+        var function_id = "unmerge_user(" + user_id + ")";
         $(checkbox).attr("onClick", function_id);
 
         //rerenders calendar with AJAX
@@ -35,21 +35,21 @@ function merge_user(user_id)
 
     }
 
-    function unmerge_user(user_id)
+function unmerge_user(user_id)
     {
 
         //removes calendar source calendars source
-        var source = "/users/" + user_id.toString() + "/events";
+        var source = "/users/" + user_id + "/events";
         $('#calendar').fullCalendar('removeEventSource', source);
 
         //changes the toggle boxes color to white and adjust text to Merge
         var divbox = "#" + user_id;
-        $(divbox.toString()).css("backgroundColor", "white");
-        $(divbox.toString()).find("p").text('Merge');
+        $(divbox).css("backgroundColor", "white");
+        $(divbox).find("p").text('Merge');
 
         //changes function of checkbox back to merge
         var checkbox = "#mergebox" + user_id;
-        var function_id = "merge_user(" + user_id.toString() + ")";
+        var function_id = "merge_user(" + user_id + ")";
         $(checkbox).attr("onClick", function_id);
 
         //rerenders calendar with AJAX
