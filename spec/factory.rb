7b461f4c03e73099a9e7951791610738d6575c5a
Bert Muthalaly <bgmuthalaly@gmail.com>
2013-01-10 02:34:06 -0600

    Created event factory and user_with_events factory. Working towards being able to create test conflicts quickly and on the fly.

--- spec/factory.rb
+++ spec/factory.rb
@@ -10,5 +10,25 @@ FactoryGirl.define do
     factory :admin do
       role "admin"
     end
+
+    factory :user_with_events do
+      # check the getting started guide for factory girl
+      # for an example of association code like this
+      ignore do
+        events_count 5
+      end
+      # add five events to this user with default attributes
+      after(:create) do |user, evaluator|
+        FactoryGirl.create_list(:event, evaluator.events_count, user: user)
+      end
+    end
+  end
+
+  factory :event do
+    title "test event"
+    sequence(:starts_at) { |n| Time.now+n.hours }
+    # TODO: need to double check that starts_at is accessible so this code works
+    ends_at { starts_at+1.hour }
+    user
   end
 end
